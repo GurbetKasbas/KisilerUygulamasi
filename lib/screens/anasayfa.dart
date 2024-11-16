6e2d992b110aeea4932b24f7c330763903efa0ad
GurbetKasbas <gurbetkasbas@gmail.com>
2024-11-16 17:07:16 +0300

    Anasayfa arayüz tasarımı tamamlandı

--- lib/screens/anasayfa.dart
+++ lib/screens/anasayfa.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:kisiler_uygulamasi/screens/kisi_detay.dart';
 import 'package:kisiler_uygulamasi/screens/kisi_kayit.dart';
@@ -43,46 +45,60 @@ class _AnasayfaState extends State<Anasayfa> {
     setState(() {});
   }
 
+  //Uygulamayı kapatma methodu
+  Future<bool> uuygulamayiKapat() async {
+    await exit(0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        //anasayfadaki geri tuşuna bastığında uygulamayı kapatır
+        leading: IconButton(
+            onPressed: () {
+              uuygulamayiKapat();
+            },
+            icon: const Icon(Icons.arrow_back)),
         backgroundColor: Colors.deepPurpleAccent,
-        title: aramaYapiliyorMu 
-        ? TextField(
-          decoration: InputDecoration( hintText: "Arama için bir şey yazın",),
-          onChanged: (aramaSonucu) {
-            print("Arama Sonucu: $aramaSonucu");
-            setState(() {
-              //Aramadan aldığım aramaSonucunu aramaKelimesine aktarıyorum
-              aramaKelimesi = aramaSonucu;
-            });
-          },
-          )
-        : Text(
-          "Kişiler Uygulaması",
-          style: TextStyle(color: Colors.white),
-        ),
-        actions: [ aramaYapiliyorMu
-        ? IconButton(
-            onPressed: () {
-            setState(() {
-              aramaYapiliyorMu = false;
-              //Arama yapılmadığı için arama içeriğini temizledim
-              aramaKelimesi = "";
-            });
-          }, 
-          icon:const Icon(Icons.cancel),
-          )
-        : IconButton(
-            onPressed: () {
-            setState(() {
-              aramaYapiliyorMu = true;
-            });
-          }, 
-          icon:const Icon(Icons.search),
-          )
+        title: aramaYapiliyorMu
+            ? TextField(
+                decoration: InputDecoration(
+                  hintText: "Arama için bir şey yazın",
+                ),
+                onChanged: (aramaSonucu) {
+                  print("Arama Sonucu: $aramaSonucu");
+                  setState(() {
+                    //Aramadan aldığım aramaSonucunu aramaKelimesine aktarıyorum
+                    aramaKelimesi = aramaSonucu;
+                  });
+                },
+              )
+            : Text(
+                "Kişiler Uygulaması",
+                style: TextStyle(color: Colors.white),
+              ),
+        actions: [
+          aramaYapiliyorMu
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      aramaYapiliyorMu = false;
+                      //Arama yapılmadığı için aramaKelimesinin içeriğini temizledik
+                      aramaKelimesi = "";
+                    });
+                  },
+                  icon: const Icon(Icons.cancel),
+                )
+              : IconButton(
+                  onPressed: () {
+                    setState(() {
+                      aramaYapiliyorMu = true;
+                    });
+                  },
+                  icon: const Icon(Icons.search),
+                )
         ],
       ),
       body: Padding(
@@ -103,7 +119,9 @@ class _AnasayfaState extends State<Anasayfa> {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => KisiDetay()));
+                                  builder: (context) => KisiDetay(
+                                        kisi: kisi,
+                                      )));
                         },
                         child: Card(
                           color: Colors.greenAccent,
@@ -140,11 +158,12 @@ class _AnasayfaState extends State<Anasayfa> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => KisiKayit()));      
-      },
-      tooltip: 'Kişi Ekle',
-      child: Icon(Icons.add , color:Colors.white),
-      backgroundColor: Colors.deepPurpleAccent,
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => KisiKayit()));
+        },
+        tooltip: 'Kişi Ekle',
+        child: Icon(Icons.add, color: Colors.white),
+        backgroundColor: Colors.deepPurpleAccent,
       ),
     );
   }
