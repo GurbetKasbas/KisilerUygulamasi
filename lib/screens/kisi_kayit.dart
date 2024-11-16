4f70688d641cb3aa11012a7c83e7a003813d682a
GurbetKasbas <gurbetkasbas@gmail.com>
2024-11-16 17:09:27 +0300

    Kişi Kayıt sayfası arayüz tasarımı tamamlandı

--- lib/screens/kisi_kayit.dart
+++ lib/screens/kisi_kayit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kisiler_uygulamasi/screens/anasayfa.dart';
 
 class KisiKayit extends StatefulWidget {
   const KisiKayit({super.key});
@@ -8,26 +9,66 @@ class KisiKayit extends StatefulWidget {
 }
 
 class _KisiKayitState extends State<KisiKayit> {
+  var tfKisiAdi = TextEditingController();
+  var tfKisiTel = TextEditingController();
+//Kişi kaydetme methodu
+  Future<void> kayit(String kisi_ad, String kisi_tel) async {
+    print("$kisi_ad - $kisi_tel rehbere eklendi");
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => Anasayfa()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Kişi Kayıt"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-
-          ],
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text(
+          "Kişi Kayıt",
+          style: TextStyle(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(
+                controller: tfKisiAdi,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  filled: true,
+                  fillColor: const Color.fromARGB(214, 68, 255, 190),
+                  hintText: "Kişi Adı",
+                ),
+              ),
+              TextField(
+                controller: tfKisiTel,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    filled: true,
+                    fillColor: const Color.fromARGB(214, 68, 255, 190),
+                    hintText: "Kişi Tel"),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.deepPurpleAccent,
         onPressed: () {
-        
-      },
-      tooltip: 'Kişi Ekle',
-      child: Icon(Icons.add),
+          kayit(tfKisiAdi.text, tfKisiTel.text);
+        },
+        tooltip: 'Kişi Kayıt',
+        icon: Icon(
+          Icons.save,
+          color: Colors.white,
+        ),
+        label: Text(
+          "Kaydet",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
